61335d3dda78613aaefaea0125277fa5482926cf
Tõnis <tonislille@gmail.com>
2023-01-12 02:06:06 +0200

    Car Fix

---
+++ TARge21Shop.ApplicationServices/Services/CarServices.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TARge21Shop.Core.Domain.Car;
+using TARge21Shop.Core.Dto;
+using TARge21Shop.Core.ServiceInterface;
+using TARge21Shop.Data;
+
+namespace TARge21Shop.ApplicationServices.Services
+{
+    public class CarServices : ICarServices
+    {
+        private readonly TARge21ShopContext _context;
+        public CarServices(TARge21ShopContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Car> Add(CarDto dto)
+        {
+            var domain = new Car()
+            {
+                Id = Guid.NewGuid(),
+                Name = dto.Name,
+                Type = dto.Type,
+                EnginePower = dto.EnginePower,
+                Weight = dto.Weight,
+                Lenght = dto.Lenght,
+                Width = dto.Width,
+                MaxSpeed = dto.MaxSpeed,
+                Mileage = dto.Mileage,
+                FuelType = dto.FuelType,
+                FuelConsumption = dto.FuelConsumption,
+                ProductionDate = dto.ProductionDate,
+            };
+
+            await _context.Cars.AddAsync(domain);
+            await _context.SaveChangesAsync();
+
+            return domain;
+
+        }
+
+        public async Task<Car> Update(CarDto dto)
+        {
+            var domain = new Car()
+            {
+                Id = dto.Id,
+                Name = dto.Name,
+                Type = dto.Type,
+                EnginePower = dto.EnginePower,
+                Weight = dto.Weight,
+                Lenght = dto.Lenght,
+                Width = dto.Width,
+                MaxSpeed = dto.MaxSpeed,
+                Mileage = dto.Mileage,
+                FuelType = dto.FuelType,
+                FuelConsumption = dto.FuelConsumption,
+                ProductionDate = dto.ProductionDate
+            };
+
+            _context.Cars.Update(domain);
+            await _context.SaveChangesAsync();
+
+            return domain;
+        }
+
+        public async Task<Car> GetUpdate(Guid id)
+        {
+            var result = await _context.Cars
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            return result;
+        }
+
+        public async Task<Car> Delete(Guid id)
+        {
+            var carId = await _context.Cars
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            _context.Cars.Remove(carId);
+            await _context.SaveChangesAsync();
+
+            return carId;
+        }
+
+        public async Task<Car> GetAsync(Guid id)
+        {
+            var result = await _context.Cars
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            return result;
+        }
+    }
+}
---
+++ TARge21Shop.Data/Migrations/20230111235642_Test1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TARge21Shop.Data.Migrations
+{
+    public partial class Test1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Spaceships");
+
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EnginePower = table.Column<int>(type: "int", nullable: false),
+                    Weight = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Lenght = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Width = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    MaxSpeed = table.Column<int>(type: "int", nullable: false),
+                    Mileage = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    FuelType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FuelConsumption = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ProductionDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FileToDatabase",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ImageTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageData = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    SpaceshipId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FileToDatabase", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Spaceship",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ModelType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SpaceshipBuilder = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PlaceOfBuild = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EnginePower = table.Column<int>(type: "int", nullable: false),
+                    LiftUpToSpaceByTonn = table.Column<int>(type: "int", nullable: false),
+                    Crew = table.Column<int>(type: "int", nullable: false),
+                    Passengers = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LaunchDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    BuildOfDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Spaceship", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cars");
+
+            migrationBuilder.DropTable(
+                name: "FileToDatabase");
+
+            migrationBuilder.DropTable(
+                name: "Spaceship");
+
+            migrationBuilder.CreateTable(
+                name: "Spaceships",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    BuiltDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CargoWeight = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Crew = table.Column<int>(type: "int", nullable: false),
+                    EnginePower = table.Column<int>(type: "int", nullable: false),
+                    FullTripsCount = table.Column<int>(type: "int", nullable: false),
+                    LastMaintenance = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    MaidenLaunch = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    MaintenanceCount = table.Column<int>(type: "int", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Passengers = table.Column<int>(type: "int", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Spaceships", x => x.Id);
+                });
+        }
+    }
+}
