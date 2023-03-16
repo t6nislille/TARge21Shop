2408547a500c96048d9dd47e98b65bf22a943140
Tõnis <tonislille@gmail.com>
2023-03-17 00:39:08 +0200

    OpenWeather Dto

---
+++ TARge21Shop.Core/Dto/WeatherDtos/OpenWeatherDto.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace TARge21Shop.Core.Dto.WeatherDtos
+{
+    public class OpenWeatherDto
+    {
+        [JsonPropertyName("weather")]
+        public List<Weathers> Weather { get; set; }
+
+        [JsonPropertyName("main")]
+        public Mains Main { get; set; }
+
+        [JsonPropertyName("wind")]
+        public Winds Wind { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+    }
+
+    public class Weathers
+    {
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+    }
+
+    public class Mains
+    {
+        [JsonPropertyName("temp")]
+        public double Temp { get; set; }
+
+        [JsonPropertyName("feels_like")]
+        public double FeelsLike { get; set; }
+
+        [JsonPropertyName("pressure")]
+        public int Pressure { get; set; }
+
+        [JsonPropertyName("humidity")]
+        public int Humidity { get; set; }
+    }
+
+    public class Winds
+    {
+        [JsonPropertyName("speed")]
+        public double Speed { get; set; }
+
+    }
+}
---
+++ TARge21Shop.Core/Dto/WeatherDtos/OpenWeatherResultsDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARge21Shop.Core.Dto.WeatherDtos
+{
+    public class OpenWeatherResultsDto
+    {
+        public string Name { get; set; }
+        public double Temperature { get; set; }
+        public double TempFeelsLike { get; set; }
+        public int Humidity { get; set; }
+        public int Pressure { get; set; }
+        public double WindSpeed { get; set; }
+        public string Description { get; set; }
+    }
+}
