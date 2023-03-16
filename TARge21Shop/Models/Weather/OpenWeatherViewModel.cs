7d86e2d58c28c4efd9fe6bb5ca0bec798689436a
Tõnis <tonislille@gmail.com>
2023-03-17 00:43:07 +0200

    OpenWeatheViewModel

---
+++ TARge21Shop/Models/Weather/OpenWeatherViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace TARge21Shop.Models.Weather
+{
+    public class OpenWeatherViewModel
+    {
+        public string Name { get; set; }
+        public double Temperature { get; set; }
+        public double TempFeelsLike { get; set; }
+        public int Humidity { get; set; }
+        public int Pressure { get; set; }
+        public double WindSpeed { get; set; }
+        public string Description { get; set; }
+    }
+}
